cc25c4691b3c3655eb77e656e73bb20e187c2bac
vijayamalini2496 <153358323+vijayamalini2496@users.noreply.github.com>
2024-03-12 11:26:38 +0530

    Create myPipeline.groovy

---
+++ vars/myPipeline.groovy
@@ -0,0 +1,61 @@
+def call() {
+    pipeline {
+        agent any
+        stages {
+            // stage('Git Checkout') {
+            //     steps {
+            //         script {
+            //             git branch: 'node-dev', url: 'https://github.com/naresh26git/multi-branch.git'
+            //         }
+            //     }
+            // }
+            stage ('Build') {
+                steps {
+                    script {
+                        sh 'npm install'
+                    }
+                }
+            }
+            // stage ('SonarQube Analysis') {
+            //     steps {
+            //         script { 
+            //             def scannerHome = tool 'sonarscanner4'
+            //             withSonarQubeEnv('sonar-pro') {
+            //                 sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=node.js-app"
+            //             }
+            //         }
+            //     }
+            // }
+            stage('Docker Build Images') {
+                steps {
+                    script {
+                        sh 'docker build -t naresh2603/multi:v1 .'
+                        sh 'docker images'
+                        sh 'docker ps'
+                    }
+                }
+            }
+            // stage('Docker Push') {
+            //     steps {
+            //         script {
+            //             withCredentials([string(credentialsId: 'dockerPass', variable: 'dockerPassword')]) {
+            //                 sh "docker login -u naresh2603 -p ${dockerPassword}"
+            //                 sh 'docker push naresh2603/multi:v2'
+            //                 sh 'docker rmi naresh2603/multi:v2'
+            //             }
+            //         }
+            //     }
+            // }
+            // stage('Deploy on k8s') {
+            //     steps {
+            //         script {
+            //             withKubeCredentials(kubectlCredentials: [[ credentialsId: 'kubernetes', namespace: 'ms' ]]) {
+            //                 sh 'kubectl apply -f kube.yaml'
+            //                 sh 'kubectl get pods -o wide'
+            //             }
+            //         }
+            //     }
+            // }
+        }
+    }
+}
